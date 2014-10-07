b1f278b4e47ad08798a986fd2ced6a98a80dc138
xaos <vladimir.p.polyakov@gmail.com>
2014-10-08 00:52:12 +0600

    editor

---
+++ src/main/java/com/googlecode/jvmvm/ui/Editor.java
@@ -0,0 +1,208 @@
+package com.googlecode.jvmvm.ui;
+
+import org.fife.rsta.ac.java.JavaCompletionProvider;
+import org.fife.rsta.ac.java.buildpath.ClasspathLibraryInfo;
+import org.fife.rsta.ui.GoToDialog;
+import org.fife.rsta.ui.search.FindDialog;
+import org.fife.rsta.ui.search.ReplaceDialog;
+import org.fife.rsta.ui.search.SearchDialogSearchContext;
+import org.fife.ui.autocomplete.AutoCompletion;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
+import org.fife.ui.rtextarea.SearchEngine;
+
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.Arrays;
+
+
+/**
+ * An application that demonstrates use of the RSTAUI project.  Please don't
+ * take this as good application design; it's just a simple example.<p>
+ * <p/>
+ * Unlike the library itself, this class is public domain.
+ *
+ * @author Robert Futrell
+ * @version 1.0
+ */
+public class Editor extends JFrame implements ActionListener {
+
+    private RSyntaxTextArea textArea;
+    private FindDialog findDialog;
+    private ReplaceDialog replaceDialog;
+
+
+    public Editor() {
+
+        initSearchDialogs();
+
+        setJMenuBar(createMenuBar());
+        JPanel cp = new JPanel(new BorderLayout());
+        setContentPane(cp);
+
+        textArea = new RSyntaxTextArea(25, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setCodeFoldingEnabled(true);
+        textArea.setBracketMatchingEnabled(true);
+        textArea.setAnimateBracketMatching(true);
+        textArea.setTabsEmulated(true);
+        textArea.setAntiAliasingEnabled(true);
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        cp.add(sp);
+
+        JavaCompletionProvider provider = new JavaCompletionProvider();
+        try {
+            provider.addJar(new ClasspathLibraryInfo(Arrays.asList(
+                    Editor.class.getName()
+            )));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        AutoCompletion ac = new AutoCompletion(provider);
+        ac.install(textArea);
+
+        setTitle("Editor");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+
+    private JMenuBar createMenuBar() {
+        JMenuBar mb = new JMenuBar();
+        JMenu menu = new JMenu("Search");
+        menu.add(new JMenuItem(new ShowFindDialogAction()));
+        menu.add(new JMenuItem(new ShowReplaceDialogAction()));
+        menu.add(new JMenuItem(new GoToLineAction()));
+        mb.add(menu);
+        return mb;
+    }
+
+
+    /**
+     * Creates our Find and Replace dialogs.
+     */
+    public void initSearchDialogs() {
+
+        findDialog = new FindDialog(this, this);
+        replaceDialog = new ReplaceDialog(this, this);
+
+        // This ties the properties of the two dialogs together (match
+        // case, regex, etc.).
+        replaceDialog.setSearchContext(findDialog.getSearchContext());
+
+    }
+
+
+    /**
+     * Listens for events from our search dialogs and actually does the dirty
+     * work.
+     */
+    public void actionPerformed(ActionEvent e) {
+
+        String command = e.getActionCommand();
+        SearchDialogSearchContext context = findDialog.getSearchContext();
+
+        if (FindDialog.ACTION_FIND.equals(command)) {
+            if (!SearchEngine.find(textArea, context).wasFound()) {
+                UIManager.getLookAndFeel().provideErrorFeedback(textArea);
+            }
+        } else if (ReplaceDialog.ACTION_REPLACE.equals(command)) {
+            if (!SearchEngine.replace(textArea, context).wasFound()) {
+                UIManager.getLookAndFeel().provideErrorFeedback(textArea);
+            }
+        } else if (ReplaceDialog.ACTION_REPLACE_ALL.equals(command)) {
+            int count = SearchEngine.replaceAll(textArea, context).getCount();
+            JOptionPane.showMessageDialog(null, count
+                    + " occurrences replaced.");
+        }
+
+    }
+
+    private class GoToLineAction extends AbstractAction {
+
+        public GoToLineAction() {
+            super("Go To Line...");
+            int c = getToolkit().getMenuShortcutKeyMask();
+            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_L, c));
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            if (findDialog.isVisible()) {
+                findDialog.setVisible(false);
+            }
+            if (replaceDialog.isVisible()) {
+                replaceDialog.setVisible(false);
+            }
+            GoToDialog dialog = new GoToDialog(Editor.this);
+            dialog.setMaxLineNumberAllowed(textArea.getLineCount());
+            dialog.setVisible(true);
+            int line = dialog.getLineNumber();
+            if (line > 0) {
+                try {
+                    textArea.setCaretPosition(textArea.getLineStartOffset(line - 1));
+                } catch (BadLocationException ble) { // Never happens
+                    UIManager.getLookAndFeel().provideErrorFeedback(textArea);
+                    ble.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+
+    private class ShowFindDialogAction extends AbstractAction {
+
+        public ShowFindDialogAction() {
+            super("Find...");
+            int c = getToolkit().getMenuShortcutKeyMask();
+            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F, c));
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            if (replaceDialog.isVisible()) {
+                replaceDialog.setVisible(false);
+            }
+            findDialog.setVisible(true);
+        }
+
+    }
+
+
+    private class ShowReplaceDialogAction extends AbstractAction {
+
+        public ShowReplaceDialogAction() {
+            super("Replace...");
+            int c = getToolkit().getMenuShortcutKeyMask();
+            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_H, c));
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            if (findDialog.isVisible()) {
+                findDialog.setVisible(false);
+            }
+            replaceDialog.setVisible(true);
+        }
+
+    }
+
+
+    public static void main(String[] a) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                new Editor().setVisible(true);
+            }
+        });
+    }
+
+}
